dd4010e0041fa09100e8b4f93d5f2bbd3c5a4274
JiangYu <yu.jiang@funplus.com>
2015-06-29 10:53:30 +0800

    update

--- scripts/curl.php
+++ scripts/curl.php
@@ -2,20 +2,27 @@
 
 date_default_timezone_set('PRC');
 
-function newRequest($url) {
+function newRequest($url)
+{
+    $host = 'farm-dev3.socialgamenet.com';
+
     $request = curl_init();
 
     curl_setopt($request, CURLOPT_URL, $url);
+    curl_setopt($request, CURLOPT_HTTPHEADER, ['Host: ' . $host]);
     curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($request, CURLOPT_TIMEOUT, 1);
+    curl_setopt($request, CURLOPT_CONNECTTIMEOUT, 1);
     curl_setopt($request, CURLOPT_HEADER, 0); // no headers in the output
 
     return $request;
 }
 
-function urlProvider($size) {
-    $urlProvider = array();
+function urlProvider($size)
+{
+    $urlProvider = [];
 
-    $url = 'http://farm-dev3.socialgamenet.com/index.html';
+    $url = 'http://10.0.64.56/index.html';
     for ($i = 0; $i < $size; $i++) {
         $urlProvider[] = $url . '?' . $i;
     }
@@ -23,7 +30,8 @@ function urlProvider($size) {
     return $urlProvider;
 }
 
-function nextTask($tasks) {
+function nextTask($tasks)
+{
     static $taskId = 0;
 
     if (count($tasks) === 0) {
@@ -36,7 +44,6 @@ function nextTask($tasks) {
     }
 
     if ($taskId > (count($tasks) - 1)) {
-//        echo 'no more tasks' . PHP_EOL;
         return null;
     }
 
@@ -47,22 +54,32 @@ function nextTask($tasks) {
     return $task;
 }
 
-function refillQueue($multiHandle, &$concurrentRequests, $url) {
+function refillQueue($multiHandle, &$concurrentRequests, $url)
+{
     $request = newRequest($url);
     $ret     = curl_multi_add_handle($multiHandle, $request);
     assert($ret === 0);
     $concurrentRequests[$url] = $request;
 }
 
-function multi($size, $concurrencyLevel) {
+/**
+ * @param $size
+ * @param $concurrencyLevel
+ * @param $verbose
+ */
+function multi($size, $concurrencyLevel, $verbose)
+{
+//    if ($concurrencyLevel > 250) {
+//        $concurrencyLevel = 250;
+//        echo 'Rate limit to 250' . PHP_EOL;
+//    }
     echo 'size = ' . $size . ', concurrency level = ' . $concurrencyLevel . PHP_EOL;
 
     $tasks = urlProvider($size);
-//    echo 'tasks: '; print_r($tasks);
 
     $multiHandle = curl_multi_init(); // init the curl Multi
 
-    $concurrentRequests = array(); // create an array for the individual curl handles
+    $concurrentRequests = []; // create an array for the individual curl handles
 
     for ($i = 0; $i < $concurrencyLevel; $i++) {
         $url = nextTask($tasks);
@@ -77,13 +94,9 @@ function multi($size, $concurrencyLevel) {
         $concurrentRequests[$url] = $request;
     }
 
-//    print_r($concurrentRequests);
-
     $fail    = 0;
     $success = 0;
     $running = null;
-
-    $stats = array();
 
     $noMoreTasks = false;
 
@@ -91,24 +104,27 @@ function multi($size, $concurrencyLevel) {
         while (($ret = curl_multi_exec($multiHandle, $running)) === CURLM_CALL_MULTI_PERFORM) {
             ;
         }
-        if ($ret != CURLM_OK) {
+        if ($ret !== CURLM_OK) {
             break;
         }
 
         // a request was just completed -- find out which one
         while ($done = curl_multi_info_read($multiHandle)) {
+            // on complete event
             $doneHandle = $done['handle'];
-//            print_r($done);
-            $info = curl_getinfo($doneHandle);
-//            print_r($info);
-            $stats[$info['url']] = $info['total_time'];
-            if ($info['http_code'] == 200) {
-                $doneUrl = $info['url'];
+            $info       = curl_getinfo($doneHandle);
+            $doneUrl    = $info['url'];
+            unset($concurrentRequests[$doneUrl]);
+
+            // on complete successfully
+            if ($info['http_code'] === 200) {
                 $success++;
 
                 $output = curl_multi_getcontent($done['handle']);
                 assert(is_string($output));
-//                echo "response len = " . strlen($output) . PHP_EOL;
+                if ($verbose) {
+                    echo 'response len = ' . strlen($output) . PHP_EOL;
+                }
 
                 if ($noMoreTasks || ($url = nextTask($tasks)) === null) {
                     $noMoreTasks = true;
@@ -124,7 +140,6 @@ function multi($size, $concurrencyLevel) {
                 // remove the curl handle that just completed
                 curl_multi_remove_handle($multiHandle, $doneHandle);
 //                echo "done url: " . $doneUrl . PHP_EOL;
-                unset($concurrentRequests[$doneUrl]);
 
 //                echo 'add new request: ' . $url . PHP_EOL;
 //                print_r($concurrentRequests);
@@ -132,37 +147,35 @@ function multi($size, $concurrencyLevel) {
                 continue;
             }
 
-            // request failed.  add error handling.
-            echo 'error: ' . curl_error($doneHandle) . PHP_EOL;
-            break 2;
-            $fail++;
+            // on complete failed
+            echo 'error[' . curl_errno($doneHandle) . '] on ' . $doneUrl . ' => ' . curl_error($doneHandle) . PHP_EOL;
+            refillQueue($multiHandle, $concurrentRequests, $doneUrl);
+            $running++;
+            curl_multi_remove_handle($multiHandle, $doneHandle);
         }
     } while ($running);
 
     curl_multi_close($multiHandle);
 
     echo PHP_EOL . "success: {$success}, fail: {$fail}" . PHP_EOL;
-//    print_r($stats);
 }
 
-function single() {
+function single()
+{
     $ch = curl_init();
 
-// set URL and other appropriate options
-    curl_setopt($ch, CURLOPT_URL, "http://www.baidu.com/");
+    curl_setopt($ch, CURLOPT_URL, 'http://www.baidu.com/');
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// grab URL and pass it to the browser
     $response = curl_exec($ch);
     echo strlen($response) . PHP_EOL;
 
-// close cURL resource, and free up system resources
     curl_close($ch);
 }
 
-$options = getopt('s:c:', array());
+$options = getopt('s:c:v', []);
 
 $size        = isset($options['s']) ? (int)$options['s'] : 1;
 $concurrency = isset($options['c']) ? (int)$options['c'] : 1;
 
-multi($size, $concurrency);
+multi($size, $concurrency, array_key_exists('v', $options));

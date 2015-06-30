30fe03a2578e7364ab07f08e2a70c032d32a5e9d
JiangYu <yu.jiang@funplus.com>
2015-06-30 14:46:01 +0800

    consume request queue, and add Notif to Persistency

--- scripts/register.php
+++ scripts/register.php
@@ -5,33 +5,72 @@
  * Date: 2015/06/24
  * Time: 7:16 PM
  */
+use BusinessEntity\Notif;
 use Repository\NotifRepoBuilder;
 
 require __DIR__ . '/../bootstrap.php';
 
-$options = getopt('', [
-    'fireTime:'
-]);
+$options = getopt('v');
 
-$fireTime = isset($options['fireTime']) ? (int)$options['fireTime'] : time();
+$requestList = getRequest();
+foreach ($requestList as $request) {
+    $notif = makeNotif($request);
+    if ($notif instanceof Notif) {
+        registerRequest($notif);
+    }
+}
 
-$repo = (new NotifRepoBuilder())->getRepo();
+function makeNotif($request)
+{
+    $reqArr = json_decode($request, true);
+    if (!is_array($reqArr)) {
+        // TODO: add error handling
+        return null;
+    }
 
-$feature  = 'feature' . mt_rand(1, 2);
-$trackRef = $feature . '_' . mt_rand(1, 10);
-$template = 'test only';
+    if (!array_key_exists('fired', $reqArr)) {
+        $reqArr['fired'] = false;
+    }
 
-$config = require __DIR__ . '/../tests/_fixture/fb.php';
-$snsid  = $config['bad']['snsid'];
-$appid  = $config['bad']['appId'];
+    $notif = (new \BusinessEntity\NotifFactory())->make($reqArr);
+    return $notif;
+}
 
-$notification           = new \BusinessEntity\Notif();
-$notification->snsid    = $snsid;
-$notification->appid    = $appid;
-$notification->feature  = $feature;
-$notification->fired    = false;
-$notification->fireTime = $fireTime;
-$notification->template = $template;
-$notification->trackRef = $trackRef;
+function getRequest($verbose = false)
+{
+    static $queue = null;
 
-$repo->register($notification);
+    if ($queue === null) {
+        $factory             = new \Persistency\Storage\RedisFactory();
+        $requestQueueSetting = [
+            'scheme'  => 'tcp',
+            'host'    => '10.0.64.56',
+            'port'    => 6379,
+            'timeout' => 5.0,
+        ];
+
+        $redis = $factory->create($requestQueueSetting);
+
+        $queue = (new \Queue\RedisQueue($redis, 'request'))->setBlockTimeout(5);
+    }
+
+    while (true) {
+        if ($verbose) {
+            echo time() . ' wait for de-queue' . PHP_EOL;
+        }
+        $request = $queue->pop();
+        if ($request) {
+            yield $request;
+        }
+    }
+}
+
+function registerRequest(Notif $notif)
+{
+    static $repo = null;
+
+    if ($repo === null) {
+        $repo = (new NotifRepoBuilder())->getRepo();
+    }
+    $repo->register($notif);
+}

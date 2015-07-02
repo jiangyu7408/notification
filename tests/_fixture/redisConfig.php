ae3cd1308fab72549264be7885d75d73ba959e85
JiangYu <yu.jiang@funplus.com>
2015-07-02 16:19:16 +0800

    add redisConfig.php as test fixture

---
+++ tests/_fixture/redisConfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Yu
+ * Date: 2015/07/02
+ * Time: 4:18 PM
+ */
+
+return [
+    'scheme'  => 'tcp',
+    'host'    => '10.0.64.56',
+    'port'    => 6379,
+    'timeout' => 5.0,
+];

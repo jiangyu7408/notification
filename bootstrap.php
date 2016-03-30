dc42184931bd6a83833bffa9e794ee5e36005eeb
JiangYu <yu.jiang@funplus.com>
2016-03-30 16:59:19 +0800

    coding standard fix: symfony2

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,8 @@
  * Created by PhpStorm.
  * User: Jiang Yu
  * Date: 2015/06/17
- * Time: 2:13 PM
+ * Time: 2:13 PM.
  */
-
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -13,7 +12,7 @@ ini_set('assert.active', '1');
 ini_set('assert.warning', '1');
 ini_set('assert.bail', '1');
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/library/dump.php';
+require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/library/dump.php';
 
 define('CONFIG_DIR', __DIR__);

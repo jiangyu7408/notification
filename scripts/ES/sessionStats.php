398f97fc36586aa3e4226f5b6bbf481cb8e5ea28
JiangYu <yu.jiang@funplus.com>
2016-04-06 16:26:16 +0800

    add file

---
+++ scripts/ES/sessionStats.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Yu
+ * Date: 2016/04/06
+ * Time: 15:21.
+ */
+use script\UidListGenerator;
+
+require __DIR__.'/../../bootstrap.php';
+
+spl_autoload_register(
+    function ($className) {
+        $classFile = str_replace('\\', '/', $className).'.php';
+        require $classFile;
+    }
+);
+
+$options = getopt('v', ['gv:']);
+
+$verbose = isset($options['v']);
+assert(isset($options['gv']), 'game version not defined');
+$gameVersion = trim($options['gv']);
+
+$lastActiveTimestamp = (new DateTime())->setTime(0, 0, 0)->getTimestamp();
+$groupedUidList = UidListGenerator::generate($gameVersion, $lastActiveTimestamp, $verbose);
+
+$stats = [];
+array_walk(
+    $groupedUidList,
+    function (array $uidList, $shardId) use (&$stats) {
+        $count = count($uidList);
+        $stats[$shardId] = $count;
+    }
+);
+
+dump($stats);
+dump('Total user count: '.array_sum($stats));

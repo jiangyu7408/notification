b16165c5eb044028d429a1beb2a82d79075a04e5
JiangYu <yu.jiang@funplus.com>
2016-04-20 17:35:54 +0800

    add unit test on InstallUidProvider

---
+++ tests/unit/DataProvider/InstallUidProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jiang Yu
+ * Date: 2016/04/20
+ * Time: 17:33.
+ */
+namespace unit\DataProvider;
+
+use Database\PdoFactory;
+use DataProvider\User\InstallUidProvider;
+
+/**
+ * Class InstallUidProviderTest.
+ */
+class InstallUidProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function test()
+    {
+        if (extension_loaded('xdebug')) {
+            return;
+        }
+        $gameVersion = 'tw';
+
+        $pool = PdoFactory::makePool($gameVersion);
+
+        $provider = new InstallUidProvider($gameVersion, $pool);
+        $groupedUidList = array_filter($provider->generate(date('Y-m-d')));
+        foreach ($groupedUidList as $shardId => $uidList) {
+            static::assertStringStartsWith('db', $shardId);
+            static::assertTrue(is_array($uidList));
+            static::assertEquals(range(0, count($uidList) - 1), array_keys($uidList));
+        }
+    }
+}

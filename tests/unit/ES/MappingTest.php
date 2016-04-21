17d5912fbffd93e3d6c31ab88d93b6411b9c3fd4
JiangYu <yu.jiang@funplus.com>
2016-04-21 17:10:52 +0800

    big updates

--- tests/unit/ES/MappingTest.php
+++ tests/unit/ES/MappingTest.php
@@ -8,10 +8,14 @@
  */
 namespace unit\ES;
 
-use Elasticsearch\Client;
+use Elastica\Bulk\Action\UpdateDocument;
+use Elastica\Client;
+use Elastica\Document;
+use Elastica\Request;
+use Elastica\Type\Mapping;
 use ESGateway\DataModel\FieldMapping\MappingFactory;
-use ESGateway\Mapping;
-use ESGateway\Type;
+use ESGateway\Factory;
+use ESGateway\User;
 
 /**
  * Class MappingTest.
@@ -21,7 +25,7 @@ class MappingTest extends \PHPUnit_Framework_TestCase
     public function testCompareMapping()
     {
         $mapping = (new MappingFactory())->make();
-        $fieldList = array_keys($mapping['properties']);
+        $fieldList = array_keys($mapping);
 
         $expectedFieldList = [
             'snsid',
@@ -77,11 +81,263 @@ class MappingTest extends \PHPUnit_Framework_TestCase
 
     public function testCreateAndDelete()
     {
-        $type = new Type('farm', 'user:test');
-        $esClient = new Client(['hosts' => ['127.0.0.1']]);
-        $instance = new Mapping($esClient, $type);
-        $instance->deleteIndex();
-        $ret = $instance->createIndex();
-        dump($ret);
+        $this->deleteIndex();
+        $this->createIndex();
+        $expectedMapping = $this->createMapping();
+        $expectedFieldList = array_keys($expectedMapping);
+        sort($expectedFieldList, SORT_STRING);
+
+        $mapping = $this->getMapping();
+        $fieldList = array_keys($mapping);
+        sort($fieldList, SORT_STRING);
+
+        $this->assertEquals($expectedFieldList, $fieldList);
+
+        $this->addAlias();
+
+        $this->addDocuments();
+        $this->updateDocuments();
+        $this->getDocuments();
+        $this->searchDocuments();
+//        $this->deleteDocuments();
+    }
+
+    public function addAlias()
+    {
+        dump(__METHOD__);
+        $client = $this->getClient();
+        $index = $client->getIndex($this->getIndexName());
+        $type = $index->getType($this->getTypeName());
+
+        $response = $index->addAlias('farm');
+        dump($response);
+    }
+
+    protected function searchDocuments()
+    {
+        dump(__METHOD__);
+        $client = $this->getClient();
+        $index = $client->getIndex($this->getIndexName());
+        $type = $index->getType($this->getTypeName());
+
+        if (false) {
+            $query = json_decode('{"query":{"bool":{"must":[{"match":{"snsid":"825763410852946"}}]}}}', true);
+            dump($query);
+
+            $path = $index->getName().'/'.$type->getName().'/_search';
+            $response = $client->request($path, Request::GET, $query);
+            $this->assertTrue($response->isOk());
+            dump($response->getEngineTime());
+            dump($response->getQueryTime());
+            dump($response->getShardsStatistics());
+            dump($response->getStatus()); // http status code
+            dump($response->getTransferInfo());
+        }
+    }
+
+    protected function updateDocuments()
+    {
+        dump(__METHOD__);
+        require_once __DIR__.'/UserProvider.php';
+        $userList = (new \UserProvider())->listUser();
+
+        $documents = array_map(
+            function (User $user) {
+                $document = new Document($user->snsid, ['name' => str_repeat('X', 10)]);
+                $document->setDocAsUpsert(true)
+                         ->setIndex($this->getIndexName())
+                         ->setType($this->getTypeName());
+
+                return $document;
+            },
+            $userList
+        );
+
+        $responseSet = $this->getClient()
+                            ->getIndex($this->getIndexName())
+                            ->updateDocuments($documents);
+        foreach ($responseSet as $response) {
+            $data = $response->getData();
+            $this->assertEquals(2, $data['_version']);
+            $updateDocument = $response->getAction();
+            $this->assertInstanceOf(UpdateDocument::class, $updateDocument);
+        }
+    }
+
+    protected function deleteDocuments()
+    {
+        dump(__METHOD__);
+        require_once __DIR__.'/UserProvider.php';
+        $userList = (new \UserProvider())->listUser();
+
+        $documents = array_map(
+            function (User $user) {
+                $document = new Document($user->snsid);
+                $document->setIndex($this->getIndexName())
+                         ->setType($this->getTypeName());
+
+                return $document;
+            },
+            $userList
+        );
+
+        $responseSet = $this->getClient()
+                            ->getIndex($this->getIndexName())
+                            ->deleteDocuments($documents);
+        foreach ($responseSet as $response) {
+            $data = $response->getData();
+            $this->assertEquals(3, $data['_version']);
+            $action = $response->getAction();
+            $this->assertEquals('delete', $action->getOpType());
+        }
+    }
+
+    protected function addDocuments()
+    {
+        dump(__METHOD__);
+        require_once __DIR__.'/UserProvider.php';
+        $userList = (new \UserProvider())->listUser();
+
+        $documents = array_map(
+            function (User $user) {
+                return $this->makeDocument($user);
+            },
+            $userList
+        );
+
+        $responseSet = $this->getClient()
+                            ->getIndex($this->getIndexName())
+                            ->addDocuments($documents);
+        foreach ($responseSet as $response) {
+            $data = $response->getData();
+            $this->assertEquals(1, $data['_version']);
+            $source = $response->getAction()
+                               ->getSource();
+            $this->assertArrayHasKey('snsid', $source);
+            $this->assertArrayHasKey('uid', $source);
+        }
+    }
+
+    protected function getDocuments()
+    {
+        dump(__METHOD__);
+        require_once __DIR__.'/UserProvider.php';
+        $userList = (new \UserProvider())->listUser();
+
+        $snsidList = array_map(
+            function (User $user) {
+                return $user->snsid;
+            },
+            $userList
+        );
+
+        array_map(
+            function ($snsid) {
+                $document = $this->getClient()
+                                 ->getIndex($this->getIndexName())
+                                 ->getType($this->getTypeName())
+                                 ->getDocument($snsid);
+                $this->assertInstanceOf(Document::class, $document);
+                $data = $document->getData();
+                $this->assertEquals($snsid, $data['snsid']);
+                $params = $document->getParams();
+                $this->assertArrayHasKey('_version', $params);
+            },
+            $snsidList
+        );
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return Document
+     */
+    protected function makeDocument(User $user)
+    {
+        $document = new Document($user->snsid, (new Factory())->toArray($user));
+        $document->setDocAsUpsert(true)
+                 ->setIndex($this->getIndexName())
+                 ->setType($this->getTypeName());
+
+        return $document;
+    }
+
+    protected function getMapping()
+    {
+        dump(__METHOD__);
+        $response = $this->getClient()
+                         ->getIndex($this->getIndexName())
+                         ->getType($this->getTypeName())
+                         ->getMapping();
+
+        $this->assertTrue(is_array($response) && isset($response[$this->getTypeName()]));
+
+        return $response[$this->getTypeName()]['properties'];
+    }
+
+    protected function createMapping()
+    {
+        dump(__METHOD__);
+        $type = $this->getClient()
+                     ->getIndex($this->getIndexName())
+                     ->getType($this->getTypeName());
+        $mapping = new Mapping();
+        $mapping->setType($type);
+
+        $mappingProperties = (new MappingFactory())->make();
+        $mapping->setProperties($mappingProperties);
+        $response = $mapping->send();
+//        dump($response);
+        $this->assertNotTrue($response->hasError());
+
+        return $mappingProperties;
+    }
+
+    protected function createIndex()
+    {
+        /** @var \Elastica\Response $response */
+        $response = $this->getClient()
+                         ->getIndex($this->getIndexName())
+                         ->create();
+        dump(__METHOD__);
+//        dump($response);
+        $this->assertNotTrue($response->hasError());
+    }
+
+    protected function deleteIndex()
+    {
+        dump(__METHOD__);
+        $exist = $this->getClient()
+                      ->getIndex($this->getIndexName())
+                      ->exists();
+        if ($exist) {
+            $response = $this->getClient()
+                             ->getIndex($this->getIndexName())
+                             ->delete();
+//            dump($response);
+            $this->assertNotTrue($response->hasError());
+        }
+    }
+
+    protected function getClient()
+    {
+        return new Client(['hosts' => ['127.0.0.1']]);
+    }
+
+    protected function getIndexName()
+    {
+        return 'farm_v2';
+    }
+
+    protected function getTypeName()
+    {
+        static $name;
+        if ($name === null) {
+//            $name = 'user:test:v'.time();
+            $name = 'user:test:v1461226876';
+            dump($name);
+        }
+
+        return $name;
     }
 }

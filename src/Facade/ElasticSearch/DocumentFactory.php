343e7dea42f02a805250d1ecb38b96cbf8d40aa8
JiangYu <yu.jiang@funplus.com>
2016-04-28 16:34:20 +0800

    upgrade Facade/ElasticSearch/DocumentFactory. add unit test.

--- src/Facade/ElasticSearch/DocumentFactory.php
+++ src/Facade/ElasticSearch/DocumentFactory.php
@@ -9,47 +9,128 @@
 namespace Facade\ElasticSearch;
 
 use Elastica\Document;
-use ESGateway\Factory;
+use ESGateway\User;
 
 /**
  * Class DocumentFactory.
  */
 class DocumentFactory
 {
+    /** @var array */
+    protected $fieldList = [];
+    /** @var Document */
+    protected $docPrototype;
+
     /**
      * DocumentFactory constructor.
      *
-     * @param string $indexName
-     * @param string $typeName
+     * @param Document $prototype
      */
-    public function __construct($indexName, $typeName)
+    public function __construct(Document $prototype)
     {
-        $this->indexName = $indexName;
-        $this->typeName = $typeName;
-        $this->userFactory = new Factory();
+        if ($prototype->getIndex() === '' || $prototype->getType() === '') {
+            throw new \InvalidArgumentException('prototype not properly inited');
+        }
+        $this->docPrototype = $prototype;
+        $fieldValuePairs = get_object_vars(new User());
+        array_walk($fieldValuePairs, function ($value, $field) {
+            $this->fieldList[$field] = gettype($value);
+        });
     }
 
     /**
      * @param string $snsid
-     * @param array  $userInfo
+     * @param array  $rawUserInfo
      *
      * @return Document
      */
-    public function make($snsid, array $userInfo)
+    public function make($snsid, array $rawUserInfo)
     {
-        assert(is_string($snsid) && strlen($snsid) > 0);
-        if (array_key_exists('snsid', $userInfo)) {
-            assert($userInfo['snsid'] === $snsid);
+        if (!isset($rawUserInfo['snsid'])) {
+            throw new \InvalidArgumentException('snsid not found: '.json_encode($rawUserInfo));
+        }
+        assert($snsid === $rawUserInfo['snsid'], print_r($rawUserInfo, true));
+
+        $payload = $this->buildPayload($rawUserInfo);
+
+        return $this->buildDocument($snsid, $payload);
+    }
+
+    /**
+     * @param array $rawUserInfo
+     *
+     * @return array
+     */
+    public function buildPayload(array $rawUserInfo)
+    {
+        $userInfo = [];
+        foreach ($this->fieldList as $field => $type) {
+            if (!array_key_exists($field, $rawUserInfo)) {
+                continue;
+            }
+            $value = $this->sanitizeValue($field, $type, $rawUserInfo);
+            $userInfo[$field] = $this->sanitizeTime($field, $value);
         }
 
-        $user = $this->userFactory->makeUser($userInfo);
-        $sanitizedUserInfo = $this->userFactory->toArray($user);
+        return array_filter($userInfo);
+    }
 
-        $document = new Document($snsid, $sanitizedUserInfo);
-        $document->setDocAsUpsert(true)
-                 ->setIndex($this->indexName)
-                 ->setType($this->typeName);
+    /**
+     * @param string $snsid
+     * @param array  $payload
+     *
+     * @return Document
+     */
+    public function buildDocument($snsid, array $payload)
+    {
+        $document = clone $this->docPrototype;
+        $document->setId($snsid)
+                 ->setData($payload)
+                 ->setDocAsUpsert(true);
 
         return $document;
+    }
+
+    /**
+     * @param string $field
+     * @param string $type
+     * @param array  $rawUserInfo
+     *
+     * @return float|int|string
+     */
+    protected function sanitizeValue($field, $type, array $rawUserInfo)
+    {
+//        dump(sprintf('%30s %20s %20s', $field, $rawUserInfo[$field], $type));
+        if ($type === 'string') {
+            return trim($rawUserInfo[$field]);
+        }
+        if ($type === 'integer') {
+            return (int) $rawUserInfo[$field];
+        }
+        if ($type === 'double') {
+            return (float) $rawUserInfo[$field];
+        }
+        throw new \LogicException(sprintf('type %s not supported', $type));
+    }
+
+    /**
+     * @param string $field
+     * @param string $input
+     *
+     * @return string
+     */
+    protected function sanitizeTime($field, $input)
+    {
+        if (!in_array($field, ['addtime', 'logintime', 'last_pay_time'])) {
+            return $input;
+        }
+        if (is_numeric($input)) {
+            return (string) date('Ymd\\THisO', $input);
+        }
+        if (is_string($input) && strpos($input, '+') === false) {
+            return date_create($input)->format('Ymd\\THisO');
+        }
+
+        return $input;
     }
 }
---
+++ tests/unit/Facade/ElasticSearchDocumentFactoryTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jiang Yu
+ * Date: 2016/04/27
+ * Time: 11:58.
+ */
+namespace unit\Facade;
+
+use Elastica\Document;
+use Facade\ElasticSearch\DocumentFactory;
+
+/**
+ * Class ElasticSearchDocumentFactoryTest.
+ */
+class ElasticSearchDocumentFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Document */
+    protected static $documentPrototype;
+
+    /**
+     *
+     */
+    public static function setUpBeforeClass()
+    {
+        require_once __DIR__.'/../ES/UserProvider.php';
+
+        self::$documentPrototype = new Document();
+        self::$documentPrototype->setIndex('farm')
+                                ->setType('user:tw');
+    }
+
+    /**
+     *
+     */
+    public function test()
+    {
+        $jsonDataList = \UserProvider::getJsonData();
+        $rawUserInfoList = [];
+        foreach ($jsonDataList as $json) {
+            $arr = json_decode($json, true);
+            $this->assertTrue(is_array($arr));
+            $rawUserInfoList[] = $arr;
+        }
+
+        $docFactory = new DocumentFactory(self::$documentPrototype);
+        foreach ($rawUserInfoList as $rawUserInfo) {
+            $payload = $docFactory->buildPayload($rawUserInfo);
+
+            $this->assertArrayNotHasKey('name', $payload, print_r($payload, true));
+            $this->assertArrayNotHasKey('email', $payload, print_r($payload, true));
+            $this->assertArrayNotHasKey('track_ref', $payload, print_r($payload, true));
+            $this->assertArrayNotHasKey('loginip', $payload, print_r($payload, true));
+
+            foreach ($payload as $field => $value) {
+                $this->assertNotEmpty($value, sprintf('field [%s] should not be empty', $field));
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function testPartial()
+    {
+        $docFactory = new DocumentFactory(self::$documentPrototype);
+
+        $userInfo = [
+            'snsid' => '100001349218797',
+            'name' => 'jiangyu',
+            'email' => 'jiangyu7408@gmail.com',
+            'country' => 'CN',
+        ];
+        $payload = $docFactory->buildPayload($userInfo);
+
+        $this->assertArrayNotHasKey('name', $payload, print_r($payload, true));
+        $this->assertArrayNotHasKey('email', $payload, print_r($payload, true));
+        $this->assertArrayHasKey('country', $payload, print_r($payload, true));
+
+        $document = $docFactory->buildDocument($userInfo['snsid'], $payload);
+        $this->assertNotEmpty($document->getIndex(), 'Bad index '.print_r($document, true));
+        $this->assertNotEmpty($document->getType(), 'Bad type '.print_r($document, true));
+    }
+}
